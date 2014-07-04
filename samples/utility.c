211d37b606bd3236a5ac31e0c6dd9626b49bda9a
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-07-05 02:15:54 +0900

    Update sample files

---
+++ samples/utility.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void test(int x, int y) {
+    if (x == y) printf("OK %d\n", x);
+    else printf("NG %d %d\n", x, y);
+}
+/* for ss.c */
+
+int vv[10] = {3, 5, 1, 8, 7, 6, 2, 10, 4, 9};
+
+int v(int i)
+{
+    return vv[i];
+}
+
+void set_v(i, x)
+{
+    vv[i] = x;
+}
