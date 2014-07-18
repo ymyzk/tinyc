2c183a4c797573a909a154ccdc367aeb99544a93
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-07-18 10:27:26 +0900

    Update sample codes

--- samples_nasm/utility.c
+++ samples_nasm/utility.c
@@ -4,16 +4,15 @@ void test(int x, int y) {
     if (x == y) printf("OK %d\n", x);
     else printf("NG %d %d\n", x, y);
 }
-/* for ss.c */
+
+/* for ss.tc */
 
 int vv[10] = {3, 5, 1, 8, 7, 6, 2, 10, 4, 9};
 
-int v(int i)
-{
+int v(int i) {
     return vv[i];
 }
 
-void set_v(i, x)
-{
+void set_v(i, x) {
     vv[i] = x;
 }
